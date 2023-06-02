f7b38a892728e653c90ce3942155f3919c7865f7
jjkip <jjlangatjose@gmail.com>
2023-06-03 00:23:50 +0300

    Animating bindings

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -10,27 +10,38 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
-        Button("Tap Me"){
-            animationAmount += 1
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3,autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap Me"){
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+//            .overlay(
+//                Circle()
+//                    .stroke(.red)
+//                    .scaleEffect(animationAmount)
+//                    .opacity(2 - animationAmount)
+//                    .animation(
+//                        .easeInOut(duration: 1)
+//                        .repeatForever(autoreverses: false),
+//                        value: animationAmount
+//                    )
+//            )
+//            .onAppear {
+//                animationAmount = 2
+//            }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+       
 //        .scaleEffect(animationAmount)
 //        .blur(radius: (animationAmount - 1) * 3)
 //        .animation(.default, value: animationAmount)
