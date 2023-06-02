b72b5642472ec4b6ca7441bf9330245f064fff7e
jjkip <jjlangatjose@gmail.com>
2023-06-02 23:29:13 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Joseph Langat on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
